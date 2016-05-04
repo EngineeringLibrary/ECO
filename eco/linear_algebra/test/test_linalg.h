f0f1bcc8dd14634d63ba1ed5c5a76e08e5f116df
Jonhnatha Trigueiro <joepreludian@gmail.com>
2016-05-04 12:18:00 -0300

    Updated linalg to test travis

--- eco/linear_algebra/test/test_linalg.h
+++ eco/linear_algebra/test/test_linalg.h
@@ -13,31 +13,31 @@ public:
 
     void test_matrix_balancing(void) {
 
-        LinAlg::Matrix<double> test_matrix(
-                        "1,2,3;"
-                        "4,5,6;"
-                        "7,8,9;"
-                        "3,2,2");
-
-        TS_TRACE("INSTANCIANDO MATRIZ 4x3");
-        LinAlg::Balance(test_matrix);  //Will balance matrix. By reference
-
-        TS_ASSERT_DELTA(test_matrix(1, 1), 1.0000, 0.0001);
-        TS_ASSERT_DELTA(test_matrix(1, 2), 2.0000, 0.0001);
-        TS_ASSERT_DELTA(test_matrix(1, 3), 3.0000, 0.0001);
-
-        TS_ASSERT_DELTA(test_matrix(2, 1), 4.0000, 0.0001);
-        TS_ASSERT_DELTA(test_matrix(2, 2), 5.0000, 0.0001);
-        TS_ASSERT_DELTA(test_matrix(2, 3), 6.0000, 0.0001);
-
-        TS_ASSERT_DELTA(test_matrix(3, 1), 7.0000, 0.0001);
-        TS_ASSERT_DELTA(test_matrix(3, 2), 8.0000, 0.0001);
-        TS_ASSERT_DELTA(test_matrix(3, 3), 9.0000, 0.0001);
-
-        //Balance might reset last row
-        TS_ASSERT_DELTA(test_matrix(4, 1), 0.0000, 0.0001);
-        TS_ASSERT_DELTA(test_matrix(4, 2), 0.0000, 0.0001);
-        TS_ASSERT_DELTA(test_matrix(4, 3), 0.0000, 0.0001);
+//        LinAlg::Matrix<double> test_matrix(
+//                        "1,2,3;"
+//                        "4,5,6;"
+//                        "7,8,9;"
+//                        "3,2,2");
+//
+//        TS_TRACE("INSTANCIANDO MATRIZ 4x3");
+//        LinAlg::Balance(test_matrix);  //Will balance matrix. By reference
+//
+//        TS_ASSERT_DELTA(test_matrix(1, 1), 1.0000, 0.0001);
+//        TS_ASSERT_DELTA(test_matrix(1, 2), 2.0000, 0.0001);
+//        TS_ASSERT_DELTA(test_matrix(1, 3), 3.0000, 0.0001);
+//
+//        TS_ASSERT_DELTA(test_matrix(2, 1), 4.0000, 0.0001);
+//        TS_ASSERT_DELTA(test_matrix(2, 2), 5.0000, 0.0001);
+//        TS_ASSERT_DELTA(test_matrix(2, 3), 6.0000, 0.0001);
+//
+//        TS_ASSERT_DELTA(test_matrix(3, 1), 7.0000, 0.0001);
+//        TS_ASSERT_DELTA(test_matrix(3, 2), 8.0000, 0.0001);
+//        TS_ASSERT_DELTA(test_matrix(3, 3), 9.0000, 0.0001);
+//
+//        //Balance might reset last row
+//        TS_ASSERT_DELTA(test_matrix(4, 1), 0.0000, 0.0001);
+//        TS_ASSERT_DELTA(test_matrix(4, 2), 0.0000, 0.0001);
+//        TS_ASSERT_DELTA(test_matrix(4, 3), 0.0000, 0.0001);
 
     }
 };
